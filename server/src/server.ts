d5de3526713e7933cebe0839d30f769ad2271652
Eric Lau <ericglau@ca.ibm.com>
2020-05-19 22:26:04 -0400

    Add EPLv2 license

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 /* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright for portions from https://github.com/microsoft/vscode-extension-samples/tree/master/lsp-sample 
+ * are held by (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
+ * 
+ * Copyright (c) 2020 Eric Lau. All rights reserved. 
+ * Licensed under the Eclipse Public License v2.0
  * ------------------------------------------------------------------------------------------ */
 
 import {
